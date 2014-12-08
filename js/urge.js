f6e3288e89711040612b9c338e028db4f84d69b3
jbermudez1 <javierbermudez@MacBook-Pro-de-Javier-2.local>
2014-12-07 20:03:38 -0700

    mas cambios

--- js/urge.js
+++ js/urge.js
@@ -3,4 +3,12 @@ $(document).on( "ready", inicio );
 function inicio (){
 	console.log("carhandop");
 	$( "nav" ).load( "menu.html" );
+	$("#filtroDos").hide();
+}
+
+$("#empresario").click(esEmpresario);
+
+function esEmpresario(){
+	var f4 = $("#filtroDos");
+	$("#filtroUno").append(f4);
 }
